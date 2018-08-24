161a642596590677f0bbd2b15ccc1c3b382405cd
BhargaviDudharejiya <dudharejiab@gmail.com>
2018-08-24 10:50:20 -0400

    day -3 update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+// import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-  hero : Hero ={
-    id: 1,
-    name: 'Spider Man'
-  };
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService ) {}
 
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes =>  this.heroes = heroes);
   }
 
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
