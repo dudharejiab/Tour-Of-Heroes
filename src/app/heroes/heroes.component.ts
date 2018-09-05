94aa6f4924c90a76aa8ee419220e7845424f1b25
BhargaviDudharejiya <dudharejiab@gmail.com>
2018-09-04 20:47:43 -0400

    changes 1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,61 +1,19 @@
-// import { Component, OnInit } from "@angular/core";
-// import { Hero } from "../hero";
-// // import { HEROES } from "../mock-heroes";
-// import { HeroService } from "../hero.service";
-// import { Observable } from "rxjs";
-// import { catchError } from "rxjs/operators";
-// @Component({
-//   selector: "app-heroes",
-//   templateUrl: "./heroes.component.html",
-//   styleUrls: ["./heroes.component.css"]
-// })
-// export class HeroesComponent implements OnInit {
-//   heroes: Hero[];
-//   // selectedHero: Hero;
-
-//   constructor(private heroService: HeroService ) { }
-
-//   ngOnInit() {
-//     this.getHeroes();
-//   }
-
-//   getHeroes(): void {
-//     this.heroService.getHeroes()
-//     .subscribe(heroes =>  this.heroes = heroes);
-//   }
-
-//   add(name: string): void {
-//     name = name.trim();
-//     if(!name) {
-//       return;
-//      }
-//     this.heroService.addHero({name} as Hero ).subscribe(hero => {
-//       this.heroes.push(hero);
-//     });
-//   }
-
-//   delete(hero: Hero): void {
-//     this.heroes = this.heroes.filter(h => h !== hero);
-//     this.heroService.deleteHero(hero).subscribe();
-//   }
-
-  
-// }
-
-import { Component, OnInit } from '@angular/core';
-
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+// import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
+  // selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService ) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -63,20 +21,23 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes =>  this.heroes = heroes);
   }
 
   add(name: string): void {
     name = name.trim();
-    if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
-      .subscribe(hero => {
-        this.heroes.push(hero);
-      });
+    if(!name) {
+      return;
+     }
+    this.heroService.addHero({name} as Hero ).subscribe(hero => {
+      this.heroes.push(hero);
+    });
   }
 
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
+
+  
 }
